8605b4e09706866e21b3957e28f70fc13bd213dc
Pavel Durov <pdurov0@gmail.com>
2016-01-13 17:27:02 +0200

    hummer time

--- www/js/angular/controllers/appController.js
+++ www/js/angular/controllers/appController.js
@@ -15,4 +15,13 @@ app.controller('AppController', ['$rootScope','$state', function($rootScope, $st
         var webview = document.getElementById("main-webview");
         webview.goBack();
     };
+
+    this.webView = document.getElementById("main-webview");
+    
+
+    this.hammertime = new Hammer(this.webView);
+    this.hammertime.on('swipe', function (ev) {
+        console.log(ev);
+    });
+
 }]);
